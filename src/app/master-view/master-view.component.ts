7d1f516618eed27f3bd96e0eff88a1c81bab1672
Pablo <pmoleri@infragistics.com>
2022-06-16 18:24:26 -0300

    Add selection of customer and orders

--- src/app/master-view/master-view.component.ts
+++ src/app/master-view/master-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Northwind_JasonService } from '../services/northwind_jason.service';
+import { BehaviorSubject } from 'rxjs';
+import { ICustomer, Northwind_JasonService } from '../services/northwind_jason.service';
 
 @Component({
   selector: 'app-master-view',
@@ -7,9 +8,16 @@ import { Northwind_JasonService } from '../services/northwind_jason.service';
   styleUrls: ['./master-view.component.scss']
 })
 export class MasterViewComponent implements OnInit {
-  public northwindJasonApiCustomers: any = null;
+  public northwindJasonApiCustomers: ICustomer[] | null = null;
   public northwindJasonApiCustomerOrdersCustomerId: any = null;
   public northwindJasonApiCustomerOrderDetailsOrderId: any = null;
+
+  public selectedCustomerId?: string;
+  public get selectedCustomer() {
+    return this.northwindJasonApiCustomers?.find((c: any) => c.customerID === this.selectedCustomerId);
+  }
+
+  public selectedOrderId?: number;
 
   constructor(
     private northwind_JasonService: Northwind_JasonService,
@@ -17,8 +25,30 @@ export class MasterViewComponent implements OnInit {
 
   ngOnInit() {
     // depending on implementation, data subscriptions might need to be unsubbed later
-    this.northwind_JasonService.getApiCustomers().subscribe(data => this.northwindJasonApiCustomers = data);
-    this.northwind_JasonService.getApiCustomerOrdersCustomerId().subscribe(data => this.northwindJasonApiCustomerOrdersCustomerId = data);
-    this.northwind_JasonService.getApiCustomerOrderDetailsOrderId().subscribe(data => this.northwindJasonApiCustomerOrderDetailsOrderId = data);
+    this.northwind_JasonService.getApiCustomers().subscribe(data => {
+      this.northwindJasonApiCustomers = data;
+      this.selectedCustomerId = data[0].customerID;
+      this.loadCustomerData(this.selectedCustomerId)
+    });
+  }
+
+  public selectCustomer(customerID: string) {
+    this.selectedOrderId = undefined;
+    this.northwindJasonApiCustomerOrdersCustomerId = [];
+    this.northwindJasonApiCustomerOrderDetailsOrderId = [];
+
+    this.selectedCustomerId = customerID;
+    this.loadCustomerData(customerID);
+  }
+
+  public loadCustomerData(customerID: string) {
+    this.northwind_JasonService.getApiCustomerOrdersCustomerId(customerID).subscribe(data => this.northwindJasonApiCustomerOrdersCustomerId = data);
+  }
+
+  public orderSelected(orderID: number) {
+    this.northwindJasonApiCustomerOrderDetailsOrderId = [];
+
+    this.selectedOrderId = orderID;
+    this.northwind_JasonService.getApiCustomerOrderDetailsOrderId(orderID).subscribe(data => this.northwindJasonApiCustomerOrderDetailsOrderId = data);
   }
 }
